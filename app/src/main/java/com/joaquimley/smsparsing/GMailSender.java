58a3e359bf0333a9e864fb10a64c4a365fd7ae26
tdomagala <C57ed1d294>
2018-09-19 15:57:35 +0100

    email added (work in progress)

--- app/src/main/java/com/joaquimley/smsparsing/GMailSender.java
+++ app/src/main/java/com/joaquimley/smsparsing/GMailSender.java
@@ -62,7 +62,7 @@ public class GMailSender extends javax.mail.Authenticator {
                 message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipients));
             Transport.send(message);
         }catch(Exception e){
-
+            e.printStackTrace();
         }
     }
 
